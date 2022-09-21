d019734b52e7e4ae81ce344c20e1006492abec77
royceonrolls <oroycerolls@gmail.com>
2022-09-21 22:35:38 +0100

    Adding pages and style

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -7,16 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController extends AbstractController{
+class VinylController extends AbstractController
+{
 
     #[Route("/")]
-    public function homepage(): Response {
+    public function homepage(): Response
+    {
         $tracks = [
-            ['song'=>'Strangers By Nature','artist'=>'Adele'],
-            ['song'=>'Easy On Me','artist'=>'Adele'],
-            ['song'=>'My Little Love','artist'=>'Adele'],
-            ['song'=>'Cry Your Heart Out','artist'=>'Adele'],
-            ['song'=>'Oh My God','artist'=>'Adele'],
+            ['song' => 'Strangers By Nature', 'artist' => 'Adele'],
+            ['song' => 'Easy On Me', 'artist' => 'Adele'],
+            ['song' => 'My Little Love', 'artist' => 'Adele'],
+            ['song' => 'Cry Your Heart Out', 'artist' => 'Adele'],
+            ['song' => 'Oh My God', 'artist' => 'Adele'],
         ];
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
@@ -25,13 +27,16 @@ class VinylController extends AbstractController{
     }
 
     #[Route("/browse/{slug}")]
-    public function browse(string $slug = null): Response {
-        if($slug) {
-            $title = "Genre: ".u(str_replace("-", " ", $slug))->title(true);
-        } else {
-            $title = "All genres";
-        }
-        return new Response($title);
+    public function browse(string $slug = null): Response
+    {
+        $genre = $slug ? u(str_replace("-", " ", $slug))->title(true) : null;
+
+
+        return $this->render(
+            'vinyl/browse.html.twig',
+            [
+                'genre' => $genre
+            ],
+        );
     }
 }
-    
