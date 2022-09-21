918902fdb378f027b0801b8a1f7c7e2c9f094fbd
royceonrolls <oroycerolls@gmail.com>
2022-09-21 15:29:40 +0100

    Working with twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,15 +2,26 @@
     
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController {
+class VinylController extends AbstractController{
 
     #[Route("/")]
     public function homepage(): Response {
-        return new Response("Title: Kings never die! really!");
+        $tracks = [
+            ['song'=>'Strangers By Nature','artist'=>'Adele'],
+            ['song'=>'Easy On Me','artist'=>'Adele'],
+            ['song'=>'My Little Love','artist'=>'Adele'],
+            ['song'=>'Cry Your Heart Out','artist'=>'Adele'],
+            ['song'=>'Oh My God','artist'=>'Adele'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route("/browse/{slug}")]
