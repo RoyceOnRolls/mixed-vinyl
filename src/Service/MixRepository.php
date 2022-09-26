4219b0c3b5143803533b00cfe052f3215f35e18d
royceonrolls <oroycerolls@gmail.com>
2022-09-26 17:06:52 +0100

    Making a service

---
+++ src/Service/MixRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Cache\CacheItemInterface;
+
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;  
+
+class MixRepository {
+
+    public function findAll(HttpClientInterface $httpClient, CacheInterface $cache): array {
+        return $cache->get('mixes_data', function(CacheItemInterface $cacheItem) use ($httpClient) {
+            $cacheItem->expiresAfter(5);
+            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            return $response->toArray();
+        });
+    }
+
+}
